ad04a579c897c9de4dce4d00403c1dd45ab9e7df
Payam <p.hoseini.ch@gmail.com>
2024-03-02 14:56:21 +0330

    Create a middleware that get user token from req with cookie parser and check if user token is correct or not.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
@@ -20,6 +21,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
@@ -29,7 +32,7 @@ app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
